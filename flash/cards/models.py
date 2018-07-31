8a3f98861f873d13bb7f5f95ac51850a9d3a6c40
kimani-njoroge <kimani064@gmail.com>
2018-07-31 12:45:04 +0300

    create course model

--- flash/cards/models.py
+++ flash/cards/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
+
 
 # Create your models here.
-
-
+class Course(models.Model):
+    COURSE= (
+        ('English', 'english'),
+        ('Mathematics', 'mathematics'),
+        ('Kiswahili', 'kiswahili'),
+        ('Social Studies', 'social studies'),
+        ('C.R.E', 'c.r.e'),
+        ('I.R.E', 'i.r.e'),
+        ('Science', 'science'),
+    )
+    name = models.CharField(max_length=30,choices=COURSE)
